c8275de26a51c51e1180a3f80850e1ebdff8168b
Sagar Wagh <waghsagar2016@gmail.com>
2024-05-20 23:21:27 +0530

    added design and testbench of half subtractor using behavioral modeling

---
+++ half_subtractor/half_subtractor_beh.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module HS_BL(ip1,ip2,borrow,difference);
+
+    input ip1,ip2;
+    output reg borrow,difference;
+
+    always@(ip1,ip2) begin
+        case({ip1,ip2})
+            2'b00 : begin borrow=0; difference=0; end
+            2'b01 : begin borrow=1; difference=1; end
+            2'b10 : begin borrow=0; difference=1; end
+            2'b11 : begin borrow=0; difference=0; end
+            default : begin borrow=0; difference=0; end
+        endcase
+    end
+
+endmodule
---
+++ half_subtractor/half_subtractor_beh_tb.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+module testbench();
+
+    reg ip1,ip2;
+    wire borrow,difference;
+    HS_BL hs(ip1,ip2,borrow,difference);
+
+    initial begin
+        $monitor("ip1=%b ip2=%b borrow=%b difference=%b",ip1,ip2,borrow,difference);
+        ip1=0; ip2=0;
+        #2 ip1=0; ip2=1;
+        #2 ip1=1; ip2=0;
+        #2 ip1=1; ip2=1;
+        #2 $finish;
+    end
+
+    initial begin 
+        $dumpfile("dump.vcd"); $dumpvars;
+    end
+
+endmodule
